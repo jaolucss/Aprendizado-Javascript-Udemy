0531da45bf7984845f343818d0f79bbce5ea71b9
João Lucas <joaolucasmoraisdossantos@gmail.com>
2026-05-27 16:54:32 -0300

    arquivo da aula 2 sobre comentários adicionado

---
+++ AulasJS/aula2.js
@@ -0,0 +1,18 @@
+// Aulas sobre tipos de comentários em JavaScript
+console.log('Comentário linha única'); // O motor ignora oque vier depois da barra dupla
+
+console.log('Linha 1');
+// console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Isso vai ser ignorado
+
+// No caso de cima a linha 2 não será exibida
+
+// O outro tipo é o comentário de bloco, em multiplas linhas, usando barra e asterisco
+
+/*
+    Nessa parte do código posso digitar um bloco de comentário
+    algo mais elaborado, explicando sobre determinada função, ou até mesmo deixar um texto
+    para o futuro, lembrando de algo importante
+*/
