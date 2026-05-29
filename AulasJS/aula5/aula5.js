3e411d27406a14b4ebad5fb222321fecb23760d8
João Lucas <joaolucasmoraisdossantos@gmail.com>
2026-05-29 10:10:38 -0300

    arquivos da aula 5 sobre constantes

---
+++ AulasJS/aula5/aula5.js
@@ -0,0 +1,15 @@
+// Não pode criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constante com um número
+// Constantes não podem conter espaços ou traços
+// Não pode modificar o valor de uma constante
+
+const nome = "João";
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+console.log(resultado);
+console.log(resultadoDuplicado);
