b724a888f6e8b93e60fa9971a3bfa39d2a717877
João Lucas <joaolucasmoraisdossantos@gmail.com>
2026-05-28 22:57:52 -0300

    arquivos da aula 3 sobre node e js com html

---
+++ AulasJS/aula3/aula3.js
@@ -0,0 +1,6 @@
+console.log('Oi');
+console.log('Trecho que será exibido no console do navegador');
+
+/*
+    alert('Isso é um alerta!'); // Só funciona no navegador, não funciona em ambientes Node.js
+*/
