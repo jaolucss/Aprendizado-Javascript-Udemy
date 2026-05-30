080ef50853b36c7396fb2f85530748ec4e5322e2
João Lucas <joaolucasmoraisdossantos@gmail.com>
2026-05-30 11:42:17 -0300

    arquivos da aula 7 sobre operadores aritmeticos de atribuição e incremento

---
+++ AulasJS/aula 7/aula7.js
@@ -0,0 +1,24 @@
+/*
+    Aritméticos
+    + Adição / Concatenação // Além de somar números também pode ser usado para concatenar strings
+    - Subtração
+    * Multiplicação
+    / Divisão
+    ** Potenciação
+    % Resto da divisão
+*/
+
+/*
+    Ordem de precedencia dos operadores
+    ( // Oque tiver dentro de parenteses tem a maior precedencia )
+    ** Potenciação
+    * Multiplicação / Divisão  % Resto da divisão
+    + Adição - Subtração
+*/
+
+/*
+    Incremento = ++
+    Decremento = --
+    Pré Incremento = ++x
+    Pós Incremento = x++
+*/
