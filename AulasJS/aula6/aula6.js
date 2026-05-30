6af9bc65a0efec3fb86bcaa31a7cff02fba2eeb4
João Lucas <joaolucasmoraisdossantos@gmail.com>
2026-05-30 10:14:20 -0300

    aula 6 sobre os tipos primitivos em js

---
+++ AulasJS/aula6/aula6.js
@@ -0,0 +1,25 @@
+// Aula sobre tipos primitivos, variáveis, string, number, boolean, undefined e null, symbol
+
+// Tipo String
+
+const nome = 'João';
+const nome1 = "João";
+const nome2 = `João`;
+
+// Tipos Number
+
+const num1 = 10;
+const num2 = 10.5;
+
+// Tipo undefined e null (não apontam para local nenhum na memória)
+
+let nomeAluno; // undefined
+const sobrenomeAluno = null; // null
+
+// Tipo boolean
+// Vai muito além de ser só verdadeiro ou falso, pode também ser utilizado para controlar o fluxo do código
+
+const aprovado = true;
+const reprovado = false;
+
+console.log(typeof nome); // Verifica o tipo da variável
